637bcdd9244e52ad93831d0ec78440b42b41cded
machonky <dev@macrobert.com>
2019-05-24 00:13:00 +0800

    Added Palladia.Test

---
+++ src/Palladia.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Palladia.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
