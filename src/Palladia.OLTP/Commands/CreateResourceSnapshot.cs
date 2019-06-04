3ac852c88e6319bf83d6f6a3463983870b30fde1
machonky <dev@macrobert.com>
2019-06-04 23:10:47 +0800

    Snapshots

---
+++ src/Palladia.OLTP/Commands/CreateResourceSnapshot.cs
@@ -0,0 +1,27 @@
+﻿using Memstate;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Palladia.OLTP.Commands
+{
+    public class CreateResourceSnapshot : Command<AuthorisationModel>
+    {
+        private readonly Resource resource;
+        private readonly Stream output;
+
+        public CreateResourceSnapshot(Resource resource, Stream output)
+        {
+            this.resource = resource;
+            this.output = output;
+        }
+
+        public override void Execute(AuthorisationModel model)
+        {
+            if (model.Resources.TryGetValue(resource.Name, out var value))
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(output, value);
+            }
+        }
+    }
+}
--- src/Palladia.OLTP/Commands/CreateSnapshot.cs
+++ src/Palladia.OLTP/Commands/CreateSnapshot.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace Palladia.OLTP.Commands
@@ -17,7 +18,8 @@ namespace Palladia.OLTP.Commands
 
         public override void Execute(AuthorisationModel model)
         {
-
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(output, model);
         }
     }
 }
