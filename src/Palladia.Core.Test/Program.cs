266b6a977de0d23c8a918be14e4c20d92e484035
machonky <dev@macrobert.com>
2019-06-04 23:19:11 +0800

    Test Projects Added

---
+++ src/Palladia.Core.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palladia.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Palladia.OLTP.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palladia.OLTP.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
